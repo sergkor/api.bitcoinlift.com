d4e9cbdfe62f195d02ef1fbfe3e219a22515461f
Sergiy Korniychuk <sergiy.korniychuk@mindstronghealth.com>
2022-06-27 11:58:40 -0700

    add LRU

--- src/parser.ts
+++ src/parser.ts
@@ -1,5 +1,6 @@
 const csv = require('fast-csv');
 const mysql = require('mysql2');
+const LRU = require('lru-cache');
 import * as fs from 'fs';
 import {config} from './config';
 
@@ -8,6 +9,7 @@ if (!fs.existsSync(file)) {
     console.log('file not found', file);
     process.exit(1);
 }
+const cache = new LRU({max: 1000000});
 try {
     const stream = fs.createReadStream(file);
     let count = 0;
@@ -18,13 +20,18 @@ try {
             })
             .on('data', data => {
                 console.log(count++, data[0]);
-                parser.pause();
-                connection.execute(
-                    'INSERT INTO address (id) VALUES (?)',
-                    [data[0]],(err, res) => {
-                        if (err) console.log(err);
-                        parser.resume();
-                    });
+                if (!cache.get(data[0])) {
+                    parser.pause();
+                    connection.execute(
+                        'INSERT INTO address (id) VALUES (?)',
+                        [data[0]],(err, res) => {
+                            if (err) {
+                                console.log(err);
+                            }
+                            cache.set(data[0], true);
+                            parser.resume();
+                        });
+                }
             })
             .on('end', (rowCount: number) => {
                 console.log("END", `Parsed ${rowCount} rows`);
